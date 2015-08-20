973c7ce308d68ede69aede0555aae0baae00891f
haykanush <hachatryan@melarn.ru>
2015-08-20 15:54:40 +0530

    company page cmmite

--- o-kompanii/index.php
+++ o-kompanii/index.php
@@ -1,4 +1,36 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("О компании");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+
+    <div class="container">
+        <div class="row">
+            <ul class="breadcrumb">
+                <li><a href="/">Главная</a></li>
+                <li class="active">О компании</li>
+            </ul>
+        </div>
+        <hr class="newshr">
+
+
+
+
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "COMPONENT_TEMPLATE" => ".default",
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "standard.php",
+                        "PATH" => "/include/about_comapny.php"
+                    )
+                ); ?>
+
+
+
+        </div>
+
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
