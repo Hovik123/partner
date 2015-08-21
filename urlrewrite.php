e0f4ac7f53e6533baedf66d534235de94ec6dd8e
haykanush <hachatryan@melarn.ru>
2015-08-21 09:59:42 +0530

    commite

--- urlrewrite.php
+++ urlrewrite.php
@@ -12,12 +12,6 @@ $arUrlRewrite = array(
 		"ID" => "bitrix:news",
 		"PATH" => "/novosti/index.php",
 	),
-	array(
-		"CONDITION" => "#^./dela/#",
-		"RULE" => "",
-		"ID" => "bitrix:news",
-		"PATH" => "/index.php",
-	),
 );
 
 ?>
